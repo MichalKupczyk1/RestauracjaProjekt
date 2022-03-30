27690f11d9d8d7a4db618227e4a86a7dadb80701
graca0 <graca333@interia.eu>
2022-03-30 21:20:08 +0200

    UOW

---
+++ ProjektTaiib/ProjektTaiib/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektTaiib.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IKelnerRepository kelner { get; }
+        IKartaDanRepository kartaDan  { get; }
+        IStolikRepository stolik{ get; }
+        ITypDaniaRepository typDania { get; }
+        IZamowienieRepository zamowienie { get; }
+        Task<int> CompleteAsync();
+        int Complete();
+    }
+}
